44f98269ccffaefb3d4c9f1eb3874e40f7535e22
sonu kaushik <sonukaushik1709@gmail.com>
2025-08-12 15:44:22 +0530

    added main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
 
   subscription_id = "9a19641f-a0da-455d-9b90-51dc0d0cdfee"
 }
-resource "azurerm_resource_group" "rg_group" {
+resource "azurerm_resource_group" "rg_group1" {
   name     = "sapna1"
   location = "westus"
 }
 
-resource "azurerm_resource_group" "rg_group" {
+resource "azurerm_resource_group" "rg_group2" {
   name     = "sapna2"
   location = "westus"
 }
